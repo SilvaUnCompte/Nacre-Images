cd0a5a8945773e4250b0a01d128898a3d226aeb4
Silvathor <silvablocks@gmail.com>
2024-09-30 19:58:20 +0200

    Update home page content, and add robot.txt

--- public/js/scroll-trigger.js
+++ public/js/scroll-trigger.js
@@ -1,18 +1,17 @@
 // Get all elements with the class 'scroll-active'
 const scrollActiveElements = document.querySelectorAll('.scroll-trigger');
 
-// Convert NodeList to Array (optional, if you need array methods)
-const scrollActiveArray = Array.from(scrollActiveElements);
-
 // Function to detect scroll level and compare with 'scroll-lv' attribute
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', scrollTrigger);
+
+function scrollTrigger() {
     const scrollPosition = window.scrollY;
 
-    scrollActiveArray.forEach(element => {
+    scrollActiveElements.forEach(element => {
         const elementPosition = element.getBoundingClientRect().top + window.scrollY - window.innerHeight * (10 / 11);
 
         if (scrollPosition >= elementPosition) {
             element.classList.add('scroll-active');
-        } 
+        }
     });
-});
+}
