13e356101fb94bf1783a76f30a45a16502112fde
Silvathor <silvablocks@gmail.com>
2024-10-06 00:10:48 +0200

    Add .htaccess file for URL rewriting

---
+++ controler/404.php
@@ -0,0 +1,9 @@
+<?php
+
+$page_name = 'Error 404';
+$description = 'You\'ve reached the end of Internet.';
+
+include($_SERVER['DOCUMENT_ROOT']."/public/html/helpers/header.php");
+include($_SERVER['DOCUMENT_ROOT']."/public/html/404.html");
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+// $_SERVER['DOCUMENT_ROOT'] = __DIR__;
+
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__ . '/controler/pages/index.php';
+        break;
+
+    case '/contact':
+        require __DIR__ . '/controler/pages/contact.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . '/controler/404.php';
+}
