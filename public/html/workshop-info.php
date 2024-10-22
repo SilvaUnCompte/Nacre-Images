44b220484e052b6c54a81f85c76e701eface2fd3
Silvathor <silvablocks@gmail.com>
2024-10-22 18:55:19 +0200

    Update cgv & legal-notice

--- public/html/workshop-info.php
+++ public/html/workshop-info.php
@@ -3,8 +3,26 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Baloo+2:400,800&display=swap">
 
 <main id="workshop-info">
-    <h1>Listes des stages</h1>
-    <div id="workshop-container">
+
+    <nav class="title-row">
+        <ul>
+            <li>
+                <input type="radio" id="tt1" name="title" value="topics" checked>
+                <label for="tt1"> Liste des Thèmes</label>
+            </li>
+            <li>
+                <input type="radio" id="tt2" name="title" value="prices">
+                <label for="tt2"> Liste des Tarifs</label>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="workshop-container"> <!-- class="animation-wrapper"> -->
+
+        <!-- <div class="particle particle-1"></div> -->
+        <!-- <div class="particle particle-2"></div> -->
+        <!-- <div class="particle particle-3"></div> -->
+        <!-- <div class="particle particle-4"></div> -->
 
         <?php
         $workshopTitle = ['Les stages classiques', 'Les thèmes exceptionnels', 'Thème sur demande', 'Stage séjour photo'];
@@ -16,15 +34,18 @@
                 return $type['regularity_type'] == $i;
             });
 
-            echo '<h2>' . $workshopTitle[$i] . '</h2>';
-            echo '<p>' . $workshopSubtitle[$i] . '</p>';
+            echo '<div class="workshop-title">';
+            echo    '<h2>' . $workshopTitle[$i] . '</h2>';
+            echo    '<hr>';
+            echo    '<p>' . $workshopSubtitle[$i] . '</p>';
+            echo '</div>';
 
             echo '<section class="row">';
             foreach ($workshopTypes_0 as $topics) {
                 // echo 'TTTTT';
                 echo '<a class="workshop-card" href="/stage/' . $topics['url'] . '">';
                 echo    '<div class="card-container">';
-                echo        '<img loading="lazy" src=\'assets/images/topics/' . $topics["img_name"] . '\')">';
+                echo        '<img loading="lazy" alt="' . $topics["img_alt"] . '" src=\'assets/images/topics/' . $topics["img_name"] . '\')">';
                 echo        '<p class="text-over">' . $topics["topic_name"] . '</p>';
                 echo    '</div>';
                 echo '</a>';
@@ -35,10 +56,10 @@
 
         <!-- TODO: mettre la bulle n'hésitez pas à demander -->
     </div>
-    <a class="bubble-text-container" href="/contact">
+    <a class="bubble-text-container transparent" href="/contact">
         <div class="bubble-text">
             <p>
-                Un thème non listé vous <br>intéresse ? Demandez moi et <br>dans la mesure du possible, je<br> tacherai de vous satisfaire !
+                Un thème non listé vous<br>intéresse ? Demandez moi et<br>dans la mesure du possible, je<br>tacherai de vous satisfaire !
                 <!-- Un thème non listé vous <br>intéresse ? N'hésitez<br> pas à demander ! //TODO choisir le text final -->
             </p>
         </div>
@@ -46,3 +67,4 @@
 </main>
 
 <br><br><br><br>
+<link rel="stylesheet" href="/public/styles/generics/star.css">
