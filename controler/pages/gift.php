6627883701f8295c51039341ea6b88bf93306dcd
Silvathor <silvablocks@gmail.com>
2025-03-17 20:34:03 +0100

    Add pricing functionality and integrate price display in gift and workshop info pages

--- controler/pages/gift.php
+++ controler/pages/gift.php
@@ -1,8 +1,13 @@
 <?php
 
+require_once($_SERVER['DOCUMENT_ROOT'] . '/database/tables/prices.php');
+
 $page_name = 'Chèque cadeau';
 $description = 'Offrez un chèque cadeau pour un stage photo avec Nacre-Images. Vous pouvez choisir le montant du chèque cadeau et le personnaliser.';
 
+$prices = Prices::getAll();
+
 include($_SERVER['DOCUMENT_ROOT'] . "/public/html/helpers/header.php");
 include($_SERVER['DOCUMENT_ROOT'] . "/public/html/gift.html");
+include($_SERVER['DOCUMENT_ROOT'] . "/public/html/price.php");
 include($_SERVER['DOCUMENT_ROOT'] . "/public/html/helpers/footer.html");
